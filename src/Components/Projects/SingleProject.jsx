7c15cc09d879efcd81a604bd10a67b23bda928ab
Sanket Kedare <sanketkedare200@gmail.com>
2024-02-22 18:42:14 +0530

    Updated

--- src/Components/Projects/SingleProject.jsx
+++ src/Components/Projects/SingleProject.jsx
@@ -20,34 +20,28 @@ const SingleProject = ({ item }) => {
               className="flex-1 w-[100%] object-cover m-auto shadow-white hover:shadow-xl border border-white rounded-xl"
             />
           </div>
+
           <motion.div
-            className="textContainer flex-1 flex flex-col lg:gap-[30px] gap-3"
+            className="textContainer flex-1 flex flex-col lg:gap-[30px]  gap-3"
             style={{ y }}
           >
-            <h2 className="lg:text-[50px] text-[30px] py-2 hover:text-sky-400">
+            <a href={item.git} className="flex items-center">
+            <h2 className="lg:text-[30px] text-[20px] py-2 hover:text-sky-400 flex  gap-2">
+              <img
+                src={githubImage}
+                className="bg-transparent mr-2 lg:w-[60px] w-[20px] h-full  my-auto"
+              />
               {item.title}
-            </h2>
-            <p className="text-gray-500 lg:text-[20px] text-sm">{item.des}</p>
-            <h1 className="grid lg:grid-cols-4 grid-cols-2">
+            </h2></a>
+            <p className="text-gray-500 lg:text-[15px] text-sm">{item.des}</p>
+            <div className="flex flex-wrap items-center justify-evenly">
               {item.skills.map((i) => (
                 <span className="w-[120px] my-2 text-center p-1 px-3 border-2 rounded-3xl shadow-inner bg-sky-300 text-black font-bold">
                   {i}
                 </span>
               ))}
-            </h1>
-            <div className="flex items-center justify-evenly gap-2">
-              
-              <a href={item.git} className="flex items-center font-bold text-lg border px-2 p-1 h-full rounded-xl hover:bg-white hover:text-black">
-
-              <img src={githubImage} className="bg-transparent mr-2 lg:w-[40px] w-[20px]"/>
-              Code
-              </a>
-              
-
-              <div className="w-[200px] rounded-lg bg-yellow-500 text-black font-bold text-lg text-center p-2">
-                Live
-              </div>
             </div>
+            <hr />
           </motion.div>
         </div>
       </div>
